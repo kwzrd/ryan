1650e484711ded976fe70d8d445d3374edff398e
kwzrd <kwzrd@pythondiscord.com>
2020-04-21 15:16:26 +0200

    Corona: avoid unsafe string strip from uncertain type

--- bot/cogs/corona.py
+++ bot/cogs/corona.py
@@ -33,7 +33,7 @@ async def cute_dict(dct: t.Dict[str, t.Union[str, int]]) -> str:
         key = f"**{better_keys.get(key.casefold(), key).capitalize()}**"
         with contextlib.suppress(ValueError, TypeError):
             val = f"{int(val):,}"
-        lines.append(f"{key}: {val.strip()}")
+        lines.append(f"{key}: {val}")
 
     return "\n".join(lines)
 
