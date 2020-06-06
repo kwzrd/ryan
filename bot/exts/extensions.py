8a953e75feee4e7353fc0e7d7c361ab502ea9547
kwzrd <kwzrd@pythondiscord.com>
2020-06-06 22:57:53 +0200

    Extensions: implement reload command

--- bot/exts/extensions.py
+++ bot/exts/extensions.py
@@ -1,11 +1,14 @@
 import logging
+import types
+import typing as t
 
 from discord.ext import commands
 
 from bot.bot import Ryan
-from bot.constants import Users
+from bot.constants import Emoji, Users
+from bot.utils import msg_error, msg_success
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 class Extensions(commands.Cog):
@@ -29,8 +32,25 @@ class Extensions(commands.Cog):
 
     @ext_group.command(name="reload", aliases=["r"])
     async def ext_reload(self, ctx: commands.Context, *, ext_name: str) -> None:
-        """Attempt to reload extension named `ext_name`."""
-        ...
+        """
+        Attempt to reload extension named `ext_name`.
+
+        This simply delegates to d.py provided utility method, so we do not need to worry
+        about much - if anything goes wrong during the reload, d.py will automatically
+        fallback on previous state, pretending that nothing happened.
+
+        The bot will report the result in `ctx`.
+        """
+        log.debug(f"Attempting to reload extension: {ext_name}")
+        try:
+            self.bot.reload_extension(ext_name)
+        except Exception as exc:
+            log.debug(f"Reload failed: {exc}")
+            response = msg_error(f"Reload failed! {exc} {Emoji.angry}")
+        else:
+            response = msg_success(f"Reloaded `{ext_name}` successfully {Emoji.ok_hand}")
+
+        await ctx.send(embed=response)
 
 
 def setup(bot: Ryan) -> None:
