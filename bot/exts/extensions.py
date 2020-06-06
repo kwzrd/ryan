92b60ab7109ad0ad898eaf16913dc959c158db4a
kwzrd <kwzrd@pythondiscord.com>
2020-06-06 22:30:27 +0200

    Extensions: draft function stubs & single cog entry point
    Let's keep it simple and only implement what we need. At the moment,
    it appears that all we'll realistically be using is reloading.

--- bot/exts/extensions.py
+++ bot/exts/extensions.py
@@ -13,6 +13,16 @@ class Extensions(commands.Cog):
     def __init__(self, bot: Ryan) -> None:
         self.bot = bot
 
+    @commands.group("extensions", aliases=["extension", "ext"])
+    async def ext_group(self, ctx: commands.Context) -> None:
+        """Entry point to the extension."""
+        ...
+
+    @ext_group.command(name="reload", aliases=["r"])
+    async def ext_reload(self, ctx: commands.Context, *, ext_name: str) -> None:
+        """Attempt to reload extension named `ext_name`."""
+        ...
+
 
 def setup(bot: Ryan) -> None:
     """Load the Extensions cog."""
