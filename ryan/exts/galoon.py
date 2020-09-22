d9008ae300a406c81436959e516bf7682467d2a0
kwzrd <kwzrd@pythondiscord.com>
2020-09-22 12:26:03 +0200

    Galoon: implement 'galoonable' helper

--- ryan/exts/galoon.py
+++ ryan/exts/galoon.py
@@ -1,5 +1,7 @@
 import logging
+import typing as t
 
+import discord
 from discord.ext import commands
 
 from ryan.bot import Ryan
@@ -8,6 +10,11 @@ from ryan.constants import Users
 USERS = {Users.gallonmate}  # Users which can be galooned
 
 log = logging.getLogger(__name__)
+
+
+def galoonable(user: t.Union[discord.Member, discord.User]) -> bool:
+    """True if `user` can be galooned."""
+    return user.id in USERS
 
 
 class Galoon(commands.Cog):
