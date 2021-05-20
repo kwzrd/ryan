e748c38d716c50d4eab2e6a39899e6e053783c04
kwzrd <git.kwzrd@gmail.com>
2021-05-20 18:45:17 +0200

    Config: improve name

--- ryan/config/secrets.py
+++ ryan/config/secrets.py
@@ -13,9 +13,9 @@ class _Secrets:
         """Load secrets from environment & set as attributes."""
         log.info("Loading secrets from environment")
 
-        keys = ("BOT_TOKEN",)
-        if any(key not in environ.keys() for key in keys):
-            raise Exception(f"Environment lacks required variables: {keys}")
+        required_keys = ("BOT_TOKEN",)
+        if any(key not in environ.keys() for key in required_keys):
+            raise Exception(f"Environment lacks required variables: {required_keys}")
 
         self.bot_token = str(environ.get("BOT_TOKEN"))
 
