74245861eaa066213cd3b50986d1e4ff8c1955c6
kwzrd <git.kwzrd@gmail.com>
2021-06-05 12:39:27 +0200

    Config: implement base class

--- ryan/config/constants.py
+++ ryan/config/constants.py
@@ -18,3 +18,26 @@ log.info(f"Chosen configuration file: '{chosen_file}'")
 
 with chosen_file.open(mode="r", encoding="UTF-8") as file:
     data = json.load(file)
+
+
+class _ConfigBase:
+    """
+    Base class for runtime config abstraction.
+
+    The base class is responsible for providing the initialisation logic. Subclasses
+    can then be entirely declarative, only specifying which attributes should be drawn
+    from the source file using annotations.
+    """
+
+    def __init__(self, section_name: str) -> None:
+        """
+        Load annotations into attributes from `section_name` in `data`.
+
+        Lookups are naive and will raise errors. CI checks should ensure that broken
+        config never reaches production.
+        """
+        log.info(f"Loading config: {type(self).__name__!r} (section: {section_name!r})")
+        section = data[section_name]
+
+        for attr_name in self.__annotations__:
+            setattr(self, attr_name, section[attr_name])
